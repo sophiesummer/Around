547f7f235b7e6e8b7484c8bd8a06b9d54f98be63
yangm2 <yangm2@illinois.edu>
2018-10-23 20:24:34 -0500

    first initailization

---
+++ main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"strconv"
+    "net/http"	
+    elastic "gopkg.in/olivere/elastic.v3"
+    "github.com/pborman/uuid"
+    "reflect"
+    "log"
+)
+
+type Location struct {
+	Lat float64 `json:"lat"` 
+	Lon float64 `json:"lon"`
+}
+
+type Post struct {
+	User string `json:"user"`
+	Message string `json:"message"`
+	Location Location `json:"location"`
+}
+
+const (
+    INDEX = "around"
+    TYPE = "post"
+    DISTANCE = "200km"
+    ES_URL = "http://35.231.185.56:9200"
+)
+
+func main() {
+		// Create a client
+        client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+        if err != nil {
+            panic(err)
+            return
+        }
+    
+        
+        exists, err := client.IndexExists(INDEX).Do()
+        if err != nil {
+            panic(err)
+        }
+        if !exists {
+          //tag location to 'geo_point'
+            mapping := `{
+                "mappings":{
+                    "post":{
+                        "properties":{
+                            "location":{
+                                "type":"geo_point"
+                            }
+                        }
+                    }
+                }
+            }`
+            _, err := client.CreateIndex(INDEX).Body(mapping).Do()
+            if err != nil {
+                // Handle error
+                panic(err)
+            }
+        }
+    
+    fmt.Println("started-service")
+	http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/search", handlerSearch)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one post request.")
+	
+	decoder := json.NewDecoder(r.Body)
+	var p Post
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)
+	}
+	
+    fmt.Fprintf(w, "Post received: %s\n", p.Message)
+    
+    id := uuid.New()
+    saveToES(&p, id)
+}
+
+// Save a post to ElasticSearch
+func saveToES(p *Post, id string) {
+    // Create a client
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	_, err = es_client.Index().
+		Index(INDEX).
+		Type(TYPE).
+		Id(id).
+		BodyJson(p).
+		Refresh(true).
+		Do()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search.")
+	
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
+	}	
+	fmt.Println("range is ", ran)
+
+	 // Create a client
+     client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+     if err != nil {
+            panic(err)
+            return
+     }
+
+     // Define geo distance query as specified in
+     // https://www.elastic.co/guide/en/elasticsearch/reference/5.2/query-dsl-geo-distance-query.html
+     q := elastic.NewGeoDistanceQuery("location")
+     q = q.Distance(ran).Lat(lat).Lon(lon)
+
+     searchResult, err := client.Search().
+            Index(INDEX).
+            Query(q).
+            Pretty(true).
+            Do()
+     if err != nil {
+            // Handle error
+            panic(err)
+     }
+
+     
+     fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+     fmt.Printf("Found a total of %d post\n", searchResult.TotalHits())
+
+     
+     var typ Post
+     var ps []Post
+     for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // instance of
+            p := item.(Post) // p = (Post) item
+            fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
+            ps = append(ps, p)
+
+     }
+
+     js, err := json.Marshal(ps)
+     if err != nil {
+            panic(err)
+            return
+     }
+
+     w.Header().Set("Content-Type", "application/json")
+     w.Header().Set("Access-Control-Allow-Origin", "*")
+     w.Write(js)
+}
+
+
+
